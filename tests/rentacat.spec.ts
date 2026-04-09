830056e654d522e574cca29284c3b191099876ab
KyleZheng11 <k1105yle@gmail.com>
2026-04-09 12:08:59 -0400

    working on "Add CI Test Workflow"

--- tests/rentacat.spec.ts
+++ tests/rentacat.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-var baseURL = 'http://localhost:8080';
+var baseURL = "http://localhost:8080";
 
 test('TEST-CONNECTION', async ({ page }) => {
   await page.goto(baseURL);
